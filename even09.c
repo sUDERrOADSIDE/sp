3997e4e5ebb0a9d17f18d1eb8bf1248140450ec9
sUDERrOADSIDE <35688749+sUDERrOADSIDE@users.noreply.github.com>
2018-02-10 10:16:04 +0530

    even09.c

---
+++ even09.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf("enter the two numbers");
+scanf("%d%d",&a,&b);
+for(c=a;a<b;a++)
+{
+if(a%2==0)
+{
+printf("%d",a);
+printf("\n");
+}
+}
+
+return 0;
+}
